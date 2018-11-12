1d0faf81aa44ba55e0b236d4a1c934b049b1f4d1
15086833944 <359308321@qq.com>
2018-11-12 15:27:44 +0800

    将检测主机任务加入到tranfer模块当中

--- check_agent.py
+++ check_agent.py
@@ -16,7 +16,7 @@ def check_agent():
         # 每隔60秒执行一次
         time.sleep(60)
         try:
-            now_time = datetime.datetime.now().strftime("%Y-%m=%d %H:%M:%S")
+            now_time = datetime.datetime.now()
             db = cx_Oracle.connect('umsproxy', 'ums1234', '172.30.130.126:1521/umsproxy')
             cur = db.cursor()
             cur.execute("select * from process_info")
@@ -25,7 +25,7 @@ def check_agent():
             for x in all_process_info:
                 time_diff = now_time - x[1]  #记录的时间与当前的时间差
                 time_diff1 = time_diff.split(':')
-                time_diff2 = time_diff1[0]*3600+time_diff1[1]*60+time_diff1[2]  #以秒钟来记录差时
+                time_diff2 = int(time_diff1[0]) * 3600 + int(time_diff1[1]) * 60 + int(float(time_diff1[2]))  #以秒钟来记录差时
                 if x[-3] == 0:  #按照分钟的定时
                     time_cycle = x[-4]*60
                     if time_cycle + 60 < time_diff2:    #判断buffer 1分钟
--- transfer.py
+++ transfer.py
@@ -241,13 +241,30 @@ def check_agent():
         # 每隔60秒执行一次
         time.sleep(60)
         try:
+            now_time = datetime.datetime.now()
             db = cx_Oracle.connect('umsproxy', 'ums1234', '172.30.130.126:1521/umsproxy')
             cur = db.cursor()
             cur.execute("select * from process_info")
             all_process_info = cur.fetchall()
-            print all_process_info
+            # print all_process_info
+            for x in all_process_info:
+                time_diff = now_time - x[1]  #记录的时间与当前的时间差
+                time_diff1 = time_diff.split(':')
+                time_diff2 = int(time_diff1[0]) * 3600 + int(time_diff1[1]) * 60 + int(float(time_diff1[2]))  #以秒钟来记录差时
+                if x[-3] == 0:  #按照分钟的定时
+                    time_cycle = x[-4]*60
+                    if time_cycle + 60 < time_diff2:    #判断buffer 1分钟
+                        print '-----------------> 发现有主机失联！失联主机biz_ip:' + x[2]
+                    else:
+                        continue
+                else:           #按照小时的定时
+                    time_cycle = x[-4] * 3600
+                    if time_cycle + 60 < time_diff2:    #判断buffer 1分钟
+                        print '-----------------> 发现有主机失联！失联主机biz_ip:' + x[2]
+                    else:
+                        continue
         except Exception as e:
-            logger.info('check_agent module connect to oracle db error:' + str(e))
+            print e
         finally:
             cur.close()
             db.close()
