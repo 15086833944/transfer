05a67142d08964280e9bd83f5b7e1269ee576d70
15086833944 <359308321@qq.com>
2018-11-13 17:46:12 +0800

    更新文件

--- threading_test.py
+++ threading_test.py
@@ -20,7 +20,8 @@ data_queue2 = Queue.Queue()  # 用来装所有的调取storeinfo接口的线程
 
 
 # 调取selectinfo接口的函数
-def fn(m):
+def fn():
+    global m
     for x in range(10):
         try:
             req = urllib2.Request(url='http://172.30.130.126:9995/selectinfo/172.30.130.126')
@@ -28,12 +29,15 @@ def fn(m):
             data = res.read()
             if not data:
                 m+=1
+            else:
+                print data
         except:
             m+=1
 
 
 # 调取storeinfo接口的函数
-def ff(n):
+def ff():
+    global n
     msg = {}
     msg["id"]=10000,
     msg["biz_ip"]="172.30.130.134",
@@ -48,15 +52,18 @@ def ff(n):
     msg["should_be"]=1
     msg["new_count"]=1
     data = urllib.urlencode(msg)
+    print data
     try:
-        req = urllib2.Request(url="http://172.30.130.126:9995/storeinfo/",data=data)
-        res = urllib2.urlopen(req)
-        fanhui = res.read()
-        if fanhui == 'ok':
-            pass
-        else:
+        for y in range(10):
+            req = urllib2.Request(url="http://172.30.130.126:9995/storeinfo/",data=data)
+            res = urllib2.urlopen(req)
+            fanhui = res.read()
             print fanhui
-            n += 1
+            if fanhui == 'ok':
+                pass
+            else:
+                print fanhui
+                n += 1
     except:
         n+=1
 
@@ -68,12 +75,13 @@ def main():
 
     pid = os.fork()
     if pid == 0:
+        global m
         while True:
             try:
                 item1 = data_queue1.get() #获取线程
                 print '调取selectinfo的线程：'+str(item1)
                 if item1:
-                    t = Thread(target=fn,args=(m,))
+                    t = Thread(target=fn)
                     t.setDaemon(True)
                     t.start()
                 if data_queue1.empty():
@@ -87,12 +95,13 @@ def main():
             print '调取selectinfo接口执行出现错误，错误数量为：'+str(m)
         sys.exit('selectinfo退出进程')
     else:
+        global n
         while True:
             try:
                 item2 = data_queue2.get() #获取线程
                 print '调取storeinfo的线程：' + str(item2)
                 if item2:
-                    t = Thread(target=ff,args=(n,))
+                    t = Thread(target=ff)
                     t.setDaemon(True)
                     t.start()
                 if data_queue2.empty():
